1ed53985eaf294c99f2745f076d0f10fe80c69da
Anton Vorush <anton.vorush@grandparade.co.uk>
2019-02-05 19:08:33 +0100

    Remove company's copyrights

--- robot/src/androidTest/kotlin/com/slesarew/robot/view/robotlist/core/ViewInteraction.kt
+++ robot/src/androidTest/kotlin/com/slesarew/robot/view/robotlist/core/ViewInteraction.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2017 William Hill. All rights reserved.
- *
- * This software is the confidential and proprietary information of William Hill or one of its
- * subsidiaries. You shall not disclose this confidential information and shall use it only in
- * accordance with the terms of the license agreement or other applicable agreement you entered into
- * with William Hill.
- *
- * WILLIAM HILL MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. WILLIAM HILL SHALL NOT BE LIABLE FOR ANY LOSSES
- * OR DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR
- * ITS DERIVATIVES.
- */
-
 package com.slesarew.robot.view.robotlist.core
 
 import android.support.test.espresso.ViewAction
