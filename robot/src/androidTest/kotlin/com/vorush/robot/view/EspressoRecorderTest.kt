babb6c0e20d94fdddb7912d8aea953dbd0ffecaa
Anton Vorush <anton.vorush@grandparade.co.uk>
2019-02-15 09:55:08 +0100

    Add Espresso-Recorder written test

---
+++ robot/src/androidTest/kotlin/com/vorush/robot/view/EspressoRecorderTest.kt
@@ -0,0 +1,164 @@
+package com.vorush.robot.view
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.replaceText
+import android.support.test.espresso.action.ViewActions.scrollTo
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withClassName
+import android.support.test.espresso.matcher.ViewMatchers.withContentDescription
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
+import android.view.ViewGroup
+import com.vorush.robot.R
+import com.vorush.robot.view.robotlist.RobotListActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class EspressoRecorderTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(RobotListActivity::class.java)
+
+    @Test
+    fun robotListActivityTest() {
+        val actionMenuItemView = onView(
+            allOf(
+                withId(R.id.add_robot), withContentDescription("Add robot"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.action_bar),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.robot_input),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.custom),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("Robot1"), closeSoftKeyboard())
+
+        val appCompatButton = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.buttonPanel),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        appCompatButton.perform(scrollTo(), click())
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.robot_list),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val textView = onView(
+            allOf(
+                withText("Robot1"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.action_bar),
+                        childAtPosition(
+                            withId(R.id.action_bar_container),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Robot1")))
+
+        val imageView = onView(
+            allOf(
+                withId(R.id.robot_image), withContentDescription("Robot1"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    @Test
+    fun manualWrittenSameTest() {
+        onView(withContentDescription(R.string.add_robot)).perform(click())
+
+        onView(withId(R.id.robot_input)).perform(ViewActions.typeText("Robot1"))
+
+        onView(withText(android.R.string.ok)).perform(click())
+
+        onView(withText("Robot1")).perform(click())
+
+        onView(withText("Robot1")).check(matches(isDisplayed()))
+
+        onView(withId(R.id.robot_image)).check(matches(withContentDescription("Robot1")))
+    }
+}
